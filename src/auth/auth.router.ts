ee6bdfb17f628ee94342b9be91e549b21c712143
Arshath <itizarsa@gmail.com>
2021-11-09 02:40:53 +0530

    add login route

---
+++ src/auth/auth.router.ts
@@ -0,0 +1,31 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+import expressAsyncHandler from "express-async-handler"
+import express, { Request, Response } from "express"
+import { validationHandler } from "../middleware/validation.middleware"
+import * as AuthService from "./auth.service"
+import { LoginDto } from "./login.dto"
+
+/**
+ * Router Definition
+ */
+
+export const authRouter = express.Router()
+
+/**
+ * Controller Definitions
+ */
+
+// POST auth/login
+
+authRouter.post(
+	"/login",
+	validationHandler(LoginDto),
+	expressAsyncHandler(async (req: Request, res: Response) => {
+		const response = await AuthService.login(req.body)
+
+		res.status(200).send(response)
+	})
+)
