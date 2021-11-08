761468e7c0b802d16272cc3408c872f795c6601c
Arshath <itizarsa@gmail.com>
2021-11-09 02:38:34 +0530

    add access-token middleware to protect routes

---
+++ src/middleware/access-token.middleware.ts
@@ -0,0 +1,33 @@
+import { Response, Request, NextFunction } from "express"
+import * as jwt from "jsonwebtoken"
+import { asyncWrapper } from "../common/util"
+
+const verifyJwt = (token: string) => {
+	return new Promise((resolve, reject) => {
+		const jwtSecret = process.env.JWT_SECRET as string
+
+		jwt.verify(token, jwtSecret, (err, decoded) => {
+			if (err) return reject(err)
+
+			return resolve(decoded)
+		})
+	})
+}
+
+export const checkAccessToken = async (req: Request, res: Response, next: NextFunction) => {
+	if (req.path === "/api/auth/login") return next()
+
+	const authHeader = req.headers["authorization"]
+
+	if (!authHeader) return res.status(401).json({ message: "Access Token required" })
+
+	const token = authHeader.split(" ")[1]
+
+	const [err, payload] = await asyncWrapper(verifyJwt(token))
+
+	if (err) return res.status(401).json({ message: "Invalid Access Token" })
+
+	res.locals.user = payload.userId
+
+	next()
+}
