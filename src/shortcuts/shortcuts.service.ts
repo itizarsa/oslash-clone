53e31fc932bbf4248f717b9dc15d480b4dd5323b
Arshath <itizarsa@gmail.com>
2021-11-08 20:02:06 +0530

    add create shortcut service

--- src/shortcuts/shortcuts.service.ts
+++ src/shortcuts/shortcuts.service.ts
@@ -11,3 +11,7 @@ import ShortcutModel, { Shortcut } from "./shortcut.model"
 export const findAll = (): Promise<Shortcut[]> => {
 	return ShortcutModel.find({}).exec()
 }
+
+export const create = (shortcut: Shortcut): Promise<Shortcut> => {
+	return new ShortcutModel(shortcut).save()
+}
