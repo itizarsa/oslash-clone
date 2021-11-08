45b0dc63e5e33cc844381a89388d63bf9aa25a90
Arshath <itizarsa@gmail.com>
2021-11-09 00:42:32 +0530

    add delete shortcut service

--- src/shortcuts/shortcuts.service.ts
+++ src/shortcuts/shortcuts.service.ts
@@ -4,6 +4,8 @@
 
 import { SearchShortcutDto } from "./search-shortcut.dto"
 import ShortcutModel, { Shortcut } from "./shortcut.model"
+import HttpException from "../common/http-exception"
+import { asyncWrapper } from "../common/util"
 
 /**
  * Service Methods
@@ -29,4 +31,18 @@ export const search = (body: SearchShortcutDto) => {
 
 export const create = (shortcut: Shortcut): Promise<Shortcut> => {
 	return new ShortcutModel(shortcut).save()
+}
+
+export const remove = async (id: string): Promise<string> => {
+	const [err, res] = await asyncWrapper(ShortcutModel.findByIdAndDelete(id).exec())
+
+	if (err) {
+		const { message } = err
+
+		throw new HttpException(500, message, err)
+	}
+
+	if (!res) throw new HttpException(404, "Shortcut Not Found")
+
+	return ""
 }
