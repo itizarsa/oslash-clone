ff1e9343f44c6d17971156abf86fc730019efb9c
Arshath <itizarsa@gmail.com>
2021-11-08 16:31:03 +0530

    apply error & not found middlewares

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import * as dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 import helmet from "helmet"
+import { errorHandler } from "./middleware/error.middleware"
+import { notFoundHandler } from "./middleware/not-found.middleware"
 
 dotenv.config()
 
@@ -26,6 +28,9 @@ const app = express()
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
+
+app.use(errorHandler)
+app.use(notFoundHandler)
 
 /**
  * Server Activation
