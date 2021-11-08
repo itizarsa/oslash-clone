954d82a205580a078bb1636676297350c95917b1
Arshath <itizarsa@gmail.com>
2021-11-09 00:26:54 +0530

    add search shortcuts router

--- src/shortcuts/shortcuts.router.ts
+++ src/shortcuts/shortcuts.router.ts
@@ -5,8 +5,9 @@
 import expressAsyncHandler from "express-async-handler"
 import express, { Request, Response } from "express"
 import { validationHandler } from "../middleware/validation.middleware"
+import { CreateShortcutDto } from "./create-shortcut.dto"
+import { SearchShortcutDto } from "./search-shortcut.dto"
 import * as ShortcutsService from "./shortcuts.service"
-import { ShortcutDto } from "./create-shortcut.dto"
 
 /**
  * Router Definition
@@ -18,7 +19,7 @@ export const shortcutsRouter = express.Router()
  * Controller Definitions
  */
 
-// GET: List all Shortcuts
+// GET shortcuts
 
 shortcutsRouter.get(
 	"/",
@@ -29,11 +30,23 @@ shortcutsRouter.get(
 	})
 )
 
-// POST: Create Shortcut
+// POST shortcuts/search
+
+shortcutsRouter.post(
+	"/search",
+	validationHandler(SearchShortcutDto),
+	expressAsyncHandler(async (req: Request, res: Response) => {
+		const response = await ShortcutsService.search(req.body)
+
+		res.status(200).send(response)
+	})
+)
+
+// POST shortcuts
 
 shortcutsRouter.post(
 	"/",
-	validationHandler(ShortcutDto),
+	validationHandler(CreateShortcutDto),
 	expressAsyncHandler(async (req: Request, res: Response) => {
 		const response = await ShortcutsService.create(req.body)
 
