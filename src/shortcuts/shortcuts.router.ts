1d59cea14623556c03ba7eb5eb3c18341a2fa5ca
Arshath <itizarsa@gmail.com>
2021-11-08 20:01:55 +0530

    add create shortcut route

--- src/shortcuts/shortcuts.router.ts
+++ src/shortcuts/shortcuts.router.ts
@@ -6,6 +6,8 @@ import * as ShortcutsService from "./shortcuts.service"
 import express, { Request, Response } from "express"
 import HttpException from "../common/http-exception"
 import { asyncWrapper } from "../common/util"
+import { validationHandler } from "../middleware/validation.middleware"
+import { ShortcutDto } from "./create-shortcut.dto"
 
 /**
  * Router Definition
@@ -17,10 +19,22 @@ export const shortcutsRouter = express.Router()
  * Controller Definitions
  */
 
-// List all Shortcuts
-
+// GET: List all Shortcuts
 shortcutsRouter.get("/", async (req: Request, res: Response) => {
 	const [shortcutErr, shortcutRes] = await asyncWrapper(ShortcutsService.findAll())
+
+	if (shortcutErr) {
+		const { message } = shortcutErr
+
+		throw new HttpException(500, message, shortcutErr)
+	}
+
+	res.status(200).send(shortcutRes)
+})
+
+// POST: Create Shortcut
+shortcutsRouter.post("/", validationHandler(ShortcutDto), async (req: Request, res: Response) => {
+	const [shortcutErr, shortcutRes] = await asyncWrapper(ShortcutsService.create(req.body))
 
 	if (shortcutErr) {
 		const { message } = shortcutErr
