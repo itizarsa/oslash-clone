1d618035a33e5f7150d0812c2143a0920b5dbe38
Arshath <itizarsa@gmail.com>
2021-11-09 12:05:05 +0530

    add create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,12 @@
  * Data Model Interfaces
  */
 
+import bcrypt from "bcrypt"
 import HttpException from "../common/http-exception"
+import { CreateUserDto } from "./create-user.dto"
+import { FindUserQuery } from "./user.interface"
 import UserModel, { User } from "./user.model"
 import { asyncWrapper } from "../common/util"
-import { FindUserQuery } from "./user.interface"
 
 /**
  * Service Methods
@@ -23,4 +25,28 @@ export const find = async (query: FindUserQuery): Promise<User> => {
 	if (!res) throw new HttpException(404, "User Not Found")
 
 	return res
+}
+
+export const create = async (body: CreateUserDto): Promise<User> => {
+	const { password, name, email } = body
+
+	const [hashErr, hash] = await asyncWrapper(bcrypt.hash(password, 10))
+
+	if (hashErr) {
+		const { message } = hashErr
+
+		throw new HttpException(500, message, hashErr)
+	}
+
+	const createUserReq = new UserModel({ name, email, password: hash }).save()
+
+	const [createUserErr, createUserRes] = await asyncWrapper(createUserReq)
+
+	if (createUserErr) {
+		const { message } = createUserErr
+
+		throw new HttpException(500, message, createUserErr)
+	}
+
+	return createUserRes
 }
